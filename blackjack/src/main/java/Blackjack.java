923cc3cb73e32d1f405359197ec6b63829c41e00
Allan Chaves Cruz <allan.9598@gmail.com>
2022-12-01 20:27:57 -0300

    blackjack parseCard method implemented

---
+++ blackjack/src/main/java/Blackjack.java
@@ -0,0 +1,65 @@
+public class Blackjack {
+
+    public int parseCard(String card) {
+
+        Integer cardvalue = 0;
+
+        switch (card) {
+
+            case "ace" -> cardvalue = 11;
+
+            case "two" -> cardvalue = 2;
+
+            case "three" -> cardvalue = 3;
+
+            case "four" -> cardvalue = 4;
+
+            case "five" -> cardvalue = 5;
+
+            case "six" -> cardvalue = 6;
+
+            case "seven" -> cardvalue = 7;
+
+            case "eight" -> cardvalue = 8;
+
+            case "nine" -> cardvalue = 9;
+
+            case "ten" -> cardvalue = 10;
+
+            case "jack" -> cardvalue = 10;
+
+            case "queen" -> cardvalue = 10;
+
+            case "king" -> cardvalue = 10;
+
+        }
+
+        return cardvalue;
+    }
+
+    public boolean isBlackjack(String card1, String card2) {
+        throw new UnsupportedOperationException("Please implement the Blackjack.isBlackjack method");
+    }
+
+    public String largeHand(boolean isBlackjack, int dealerScore) {
+        throw new UnsupportedOperationException("Please implement the Blackjack.largeHand method");
+    }
+
+    public String smallHand(int handScore, int dealerScore) {
+        throw new UnsupportedOperationException("Please implement the Blackjack.smallHand method");
+    }
+
+    // FirstTurn returns the semi-optimal decision for the first turn, given the cards of the player and the dealer.
+    // This function is already implemented and does not need to be edited. It pulls the other functions together in a
+    // complete decision tree for the first turn.
+    public String firstTurn(String card1, String card2, String dealerCard) {
+        int handScore = parseCard(card1) + parseCard(card2);
+        int dealerScore = parseCard(dealerCard);
+
+        if (20 < handScore) {
+            return largeHand(isBlackjack(card1, card2), dealerScore);
+        } else {
+            return smallHand(handScore, dealerScore);
+        }
+    }
+}
