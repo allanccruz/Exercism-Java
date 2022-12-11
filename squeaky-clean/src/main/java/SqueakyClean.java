f3aea0bb09be1887a6457941a86017d8ad807780
Allan Chaves Cruz <allan.9598@gmail.com>
2022-12-11 13:20:59 -0300

    squeaky-clean: task 1, 2 and 3 resolved

---
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -0,0 +1,33 @@
+import static java.util.Collections.replaceAll;
+
+class SqueakyClean {
+    static String clean(String identifier) {
+
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < identifier.length(); i++) {
+
+            char c = identifier.charAt(i);
+
+            if (Character.isSpaceChar(c)) {
+                str.append('_');
+            }
+
+            else if (Character.isISOControl(c)) {
+                str.append("CTRL");
+            }
+
+            else if (c == '-') {
+                identifier = identifier.replace(identifier.charAt(i+1), Character.toUpperCase(identifier.charAt(i+1)));
+            }
+
+            else {
+                str.append(c);
+            }
+        }
+
+        return str.toString();
+
+    }
+
+}
